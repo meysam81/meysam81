bf9b884ea3585bbcf62dcb9b93da5776504c1f6d
Meysam Azad <git@meysam.io>
2025-10-19 22:20:16 +0700

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: "https://meysam.io",
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: "auto",
+    minify: true,
   },
   compressHTML: true,
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        "@": path.resolve(__dirname, "./src"),
       },
+    },
+    build: {
+      minify: "esbuild",
+      cssMinify: true,
     },
   },
 });
