13ee8ba59f52987df1cdb97ff3e87c4b921122e8
Meysam Azad <git@meysam.io>
2025-10-31 14:03:33 +0700

    feat: sort blog post collections from source

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -5,11 +5,15 @@ export function filterPublishedPosts(
 ): CollectionEntry<"blog">[] {
   var now = new Date();
 
-  return posts.filter(function filterPosts(post) {
-    if (import.meta.env.DEV) {
-      return true;
-    }
-    var isFuture = post.data.pubDate > now;
-    return !post.data.draft && !isFuture;
-  });
+  return posts
+    .filter(function filterPosts(post) {
+      if (import.meta.env.DEV) {
+        return true;
+      }
+      var isFuture = post.data.pubDate > now;
+      return !post.data.draft && !isFuture;
+    })
+    .sort(function sortPosts(a, b) {
+      return b.data.pubDate.getTime() - a.data.pubDate.getTime();
+    });
 }
