bfc3b1a95af9f6df9e7ead8b66f8ae7c0c33a58d
Meysam Azad <git@meysam.io>
2025-11-30 13:59:34 +0700

    fix: write the next blog post

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,5 +1,8 @@
 import type { CollectionEntry } from "astro:content";
 
+// when duplicate slugs are found, add a deterministic 6-char hash suffix to make them unique
+var slugToFilepath: Record<string, string> = {};
+
 export function filterPublishedPosts(
   posts: CollectionEntry<"blog">[]
 ): CollectionEntry<"blog">[] {
@@ -12,6 +15,28 @@ export function filterPublishedPosts(
       }
       var isFuture = post.data.pubDate > now;
       return !post.data.draft && !isFuture;
+    })
+    .map(function mapPosts(post) {
+      var slug = post.data.slug;
+      if (slug in slugToFilepath) {
+        var existingPath = slugToFilepath[slug];
+        var combined = slug + existingPath;
+        var hash = Array.from(combined)
+          .reduce(function hashReducer(acc, char) {
+            return acc + char.charCodeAt(0);
+          }, 0)
+          .toString(16)
+          .slice(-6);
+        slug = `${slug}-${hash}`;
+      }
+      slugToFilepath[slug] = post.filePath;
+      return {
+        ...post,
+        data: {
+          ...post.data,
+          slug: slug,
+        },
+      };
     })
     .sort(function sortPosts(a, b) {
       return b.data.pubDate.getTime() - a.data.pubDate.getTime();
